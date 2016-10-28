1e3e7cecb628c42ccfe9c114f17117b630f785f5
Alex Wilcken Pratt <alex.w.pratt@gmail.com>
2016-10-27 18:15:11 -0600

    changes to mission controller and view

--- MissionSite/App_Start/RouteConfig.cs
+++ MissionSite/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MissionSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Mission",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Mission", action = "Index", mission = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- MissionSite/Controllers/MissionController.cs
+++ MissionSite/Controllers/MissionController.cs
@@ -11,7 +11,36 @@ namespace MissionSite.Controllers
         // GET: Mission
         public ActionResult Index(string mission)
         {
-
+            switch (mission)
+            {
+                case "Colombia Bogota North":
+                    ViewBag.MissionName = mission;
+                    ViewBag.MissionPresidentName = "President Mark F. Andelin";
+                    ViewBag.MissionAddress = "Colombia Bogota North Mission<br>Calle 72 #10-07, Oficina 1001<br>Edificio Liberty Seguros<br>Bogota, Cundinamarca<br>Colombia";
+                    ViewBag.Language = "Spanish";
+                    ViewBag.Climate = "Subtropical Highland";
+                    ViewBag.DominateReligion = "Catholic / Evangelical";
+                    ViewBag.Symbol = "colombia_bogota_north.png";
+                    break;
+                case "Brasil Belem":
+                    ViewBag.MissionName = mission;
+                    ViewBag.MissionPresidentName = "President José C. Scisci";
+                    ViewBag.MissionAddress = "Brazil Belem Mission<br>Av. Narzare, 532 Sala 412 4˚ Andar<br>Nazaré Royal Trade Center<br>66035-135 Belém-PA";
+                    ViewBag.Language = "Portuguese";
+                    ViewBag.Climate = "Tropical";
+                    ViewBag.DominateReligion = "Catholic";
+                    ViewBag.Symbol = "brasil_belem.png";
+                    break;
+                case "Brasil Curitiba":
+                    ViewBag.MissionName = mission;
+                    ViewBag.MissionPresidentName = "President Douglas K. Hart";
+                    ViewBag.MissionAddress = "Escritório da Missão Brasil Curitiba<br>Av. Iguaçu 1460<br>Bairro Água Verde<br>CEP: 80250-190<br>Curitiba –PR";
+                    ViewBag.Language = "Portuguese";
+                    ViewBag.Climate = "Subtropical Highland";
+                    ViewBag.DominateReligion = "Catholic / Evangelical";
+                    ViewBag.Symbol = "brasil_curitiba.png";
+                    break;
+            }
 
             return View();
         }
