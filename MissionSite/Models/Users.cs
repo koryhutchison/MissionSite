f31ea84e7733e0fa8586f79c4ef776d4f9fa7f9f
Alex Wilcken Pratt <alex.w.pratt@gmail.com>
2016-12-08 17:59:14 -0700

    added validation and regex

--- MissionSite/Models/Users.cs
+++ MissionSite/Models/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,9 +13,22 @@ namespace MissionSite.Models
     {
         [Key]
         public decimal UserID { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DisplayName("Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Please Enter Correct Email Address")]
         public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DisplayName("Password")]
         public string UserPassword { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
         public string UserFirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
         public string UserLastName { get; set; }
     }
 }
